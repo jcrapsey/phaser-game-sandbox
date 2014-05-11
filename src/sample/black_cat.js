5656e1ede7c6209d887d19549e078d431e98468e
Jeremy Crapsey <jcrapsey@gmail.com>
2014-05-11 01:47:59 -0400

    add sample game project

---
+++ src/sample/black_cat.js
@@ -0,0 +1,66 @@
+var BlackCat;
+(function(){
+  BlackCat = function(){
+    var s = GAME.game.add.sprite(0, 0, "black_cat");
+
+    s.physicsBodyType = Phaser.Physics.ARCADE;
+    GAME.game.physics.enable(s, Phaser.Physics.ARCADE);
+
+    s.scale.x = 0.5;
+    s.scale.y = 0.5;
+
+    s.anchor.x = 0.5;
+    s.anchor.y = 1;
+
+    s.animations.add("ball", [0], 0, false);
+    s.animations.add("pickup", [1], 0, false);
+    s.animations.add("blink", [4, 2, 3, 4], 6, false);
+    s.animations.add("idle", [4], 0, false);
+
+    s.animations.play("blink");
+
+    s.body.bounce.y = 0.6;
+    s.body.gravity.y = 9.5;
+    s.body.collideWorldBounds = true;
+
+    this.sprite = s;
+  };
+
+  BlackCat.prototype.update = function(){
+    var s = this.sprite;
+    var cursors = GAME.cursors;
+
+    if(s.body.velocity.y > 0){
+      s.animations.play("ball");
+    }else{
+      s.animations.play("idle");
+    }
+
+    if(s.body.velocity.x > 0){
+      s.scale.x = -0.5;
+    }
+
+    if(s.body.velocity.x < 0){
+      s.scale.x = 0.5;
+    }
+
+    if(cursors.left.isDown){
+      s.body.velocity.x = -50;
+    }
+
+    if(cursors.right.isDown){
+      s.body.velocity.x = 50;
+    }
+
+    if(
+      !cursors.right.isDown &&
+      !cursors.left.isDown
+    ){
+      s.body.velocity.x = 0;
+    }
+
+    if(cursors.up.isDown){
+      s.body.velocity.y = 500;
+    }
+  };
+})();
--- src/sample/index.js
+++ src/sample/index.js
@@ -1,3 +1,28 @@
 (function(){
-  console.log("hello world");
+  var blackCat;
+
+  var preload = function preload(){
+    GAME.game.load.spritesheet("black_cat", "assets/black_cat.png", 128, 128, 6);
+  };
+
+  var create = function create(){
+    var game = GAME.game;
+
+    GAME.cursors = game.input.keyboard.createCursorKeys();
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.gravity.y = 250;
+
+    blackCat = new BlackCat();
+  };
+
+  var update = function update(){
+    blackCat.update();
+  };
+
+  GAME.game = new Phaser.Game(480, 230, Phaser.AUTO, "game-container", {
+    preload: preload,
+    create:  create,
+    update: update
+  });
 }());
