abd08ea1a50fdcbcd2e12604d7999014f8a7ae8a
Jeremy Crapsey <jcrapsey@gmail.com>
2014-05-15 01:20:20 -0400

    gulpfile: add tasks to support development

--- gulpfile.js
+++ gulpfile.js
@@ -13,3 +13,64 @@ var concat     = require('gulp-concat');
 var bower_includes = [
   'phaser/phaser.js'
 ];
+// Tasks for development directory "lib"
+gulp.task('clean:lib', function(){
+  return gulp.src([
+    'lib/**/*'
+  ]).pipe(clean());
+});
+
+gulp.task('make:lib', ['clean:lib'], function(){
+  gulp.src(bower_includes, {cwd: 'bower_components'})
+    .pipe(concat('third_party.js'))
+    .pipe(gulp.dest('lib'));
+  gulp.src(['src/**/*.*', '!src/**/*.js'], {base: 'src'})
+    .pipe(gulp.dest('lib'));
+
+  glob("*/index.js", {
+    cwd: "src/"
+  }, function (err, files){
+    files.forEach(function(file){
+      return browserify(path.join(__dirname, "src", file))
+        .bundle({debug: true})
+        .pipe(source(file))
+        .pipe(gulp.dest('lib'));
+    });
+  });
+});
+
+gulp.task('develop', ['make:lib'], function(){
+  var port = process.env.PORT || 3000;
+  var app = connect();
+  var watcher = gulp.watch('**/*.*', {cwd: './src/'});
+
+  watcher.on('change', function(file){
+    var src   = path.join(__dirname, 'src');
+    var ext   = path.extname(file.path);
+    var fPath = file.path.replace(src, '').split(path.sep).slice(1);
+    var proj  = fPath[0];
+    var type  = file.type;
+
+    fPath = fPath.join(path.sep);
+
+    if(ext === ".js"){
+      browserify(path.join(__dirname, 'src', proj))
+        .bundle({debug: true})
+        .pipe(source(path.join(proj, 'index.js')))
+        .pipe(gulp.dest('lib'));
+      return;
+    }
+
+    if(type === "changed" || type === "added"){
+      gulp.src(file.path, {base: 'src'}).pipe(gulp.dest('lib'));
+    }
+
+    if(type === "deleted"){
+      gulp.src(fPath, {cwd: 'lib'}).pipe(clean());
+    }
+  });
+
+  console.log('server on port '+port);
+  app.use(connect.static('lib'));
+  http.createServer(app).listen(port);
+});
